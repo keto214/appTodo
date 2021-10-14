9518ca294cef0077e8a6af5d6b0aa171a28cede8
keto214 <74033733+keto214@users.noreply.github.com>
2021-10-14 10:10:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import TodoList from './TodoList';
-import {Router} from "react-router";
+
 function App() {
   return (
     <div className='todo-app'>
